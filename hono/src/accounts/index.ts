02b19ef9744e8a58a7a26ce98de84c908ae15637
Jiangshuuu <globelex65@gmail.com>
2024-11-06 21:22:26 +0800

    upd account table

--- hono/src/accounts/index.ts
+++ hono/src/accounts/index.ts
@@ -1,4 +1,6 @@
 import { Hono } from "hono"
+import db from "../db"
+import { accountsTable } from "../db/schema"
 
 const app = new Hono()
 
@@ -6,6 +8,19 @@ app.get('/', (c) => {
   return c.json({
     ok: true,
     message: 'Hello Accounts!',
+  })
+})
+
+app.post('/', async (c) => {
+  const info = {
+    name: 'John',
+    age: 20,
+    email: 'john@example.com',
+  }
+  await db.insert(accountsTable).values(info)
+  console.log('success')
+  return c.json({
+    ok: true,
   })
 })
 
