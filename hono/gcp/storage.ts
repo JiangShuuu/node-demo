442e174b3364edbc08f4a90f958842842b0d4eb0
Jiangshuuu <globelex65@gmail.com>
2024-12-10 20:47:55 +0800

    upd upload file

--- hono/gcp/storage.ts
+++ hono/gcp/storage.ts
@@ -7,15 +7,30 @@ export const googleStorage = new Storage({
 
 export const googleBucket = googleStorage.bucket(process.env.GOOGLE_STORAGE_BUCKET_NAME || '');
 
-export async function uploadGoogleFile(base64Data: string, destFileName: string, generationMatchPrecondition: number) {
+export async function uploadGoogleFile(base64Data: string, destFileName: string, generationMatchPrecondition: number) {  
   const buffer = Buffer.from(base64Data, 'base64');
 
   const url = `https://storage.googleapis.com/${process.env.GOOGLE_STORAGE_BUCKET_NAME ?  process.env.GOOGLE_STORAGE_BUCKET_NAME + '/': ''}${destFileName}`;
 
   const file = googleBucket.file(destFileName);
-  await file.save(buffer, {
-    preconditionOpts: { ifGenerationMatch: generationMatchPrecondition }
-  });
+
+  try {
+    // 如果該檔案已經存在，則使用該檔案的 generation 來進行上傳
+    const [metadata] = await file.getMetadata();
+    await file.save(buffer, {
+      preconditionOpts: { 
+        ifGenerationMatch: metadata.generation 
+      }
+    });
+  } catch (error) {
+    // 如果該檔案不存在，則使用 0 代表新文件
+    await file.save(buffer, {
+      preconditionOpts: { 
+        ifGenerationMatch: 0 
+      }
+    });
+  }
+  
   await file.makePublic();
   return url;
 }
