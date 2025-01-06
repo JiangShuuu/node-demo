ff3cf48dee95785bedcee4ded486134c1627c232
Jiangshuuu <globelex65@gmail.com>
2025-01-06 11:28:59 +0800

    upd pagination

--- hono-prisma/src/controllers/posts/pagination/index.ts
+++ hono-prisma/src/controllers/posts/pagination/index.ts
@@ -4,14 +4,30 @@ import prisma from "../../../lib/prisma";
 const pagination = new Hono()
 
 const getPostsPagination = async (c: Context) => {
+  const page = Number(c.req.query('page')) || 1
+  const size = Number(c.req.query('size')) || 10
+
   const posts = await prisma.post.findMany({
-    skip: 10,
-    take: 10
+    skip: (page - 1) * size, // 跳過前 n 個
+    take: size // 取 n 個
+  })
+
+  return c.json(posts)
+}
+
+const getCursorPagination = async (c: Context) => {
+  const cursor = Number(c.req.query('cursor')) || 1
+  const size = Number(c.req.query('size')) || 10
+
+  const posts = await prisma.post.findMany({
+    cursor: { id: cursor }, // 從 id 幾開始
+    take: size // 取 n 個
   })
 
   return c.json(posts)
 }
 
 pagination.get('/', getPostsPagination)
+pagination.get('/cursor', getCursorPagination)
 
 export default pagination
