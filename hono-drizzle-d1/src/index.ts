4727fac34a47b2d55a18829a37966bd2000d0419
Jiangshuuu <globelex65@gmail.com>
2025-01-14 11:11:55 +0800

    add drizzle-d1

---
+++ hono-drizzle-d1/src/index.ts
@@ -0,0 +1,28 @@
+import { Hono } from 'hono'
+import { drizzle } from 'drizzle-orm/d1'
+import { posts } from './db/schema'
+
+export type Env = {
+  MY_VAR: string
+  PRIVATE: string
+  DB: D1Database
+}
+
+const app = new Hono<{ Bindings: Env }>()
+
+app.get('/posts', async (c) => {
+  const db = drizzle(c.env.DB)
+  const result = await db.select().from(posts).all()
+
+  return c.json(result)
+}).post('/posts', async (c) => {
+  const db = drizzle(c.env.DB)
+
+  const { title, content } = await c.req.json()
+
+  const result = await db.insert(posts).values({ title, content }).returning()
+
+  return c.json(result)
+})
+
+export default app
